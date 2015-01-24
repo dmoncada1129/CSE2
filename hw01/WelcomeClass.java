cfc93ebbf6200c8f010b7726510d09f3c4244157
dmoncada1129 <dam318@lehigh.edu>
2015-01-24 00:38:29 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Daniel Moncada
+//Welcome Class Java Program
+//
+// first compile the program
+//       javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+        
+// add main method
+    public static void main(String[ ] args) {
+    
+//  print the statement with box around it
+        System.out.println("-----------"); 
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-D--A--M--3--1--8->");
+        System.out.println("\\ /\\ /\\ /\\ /\\ / \\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("This winter break I managed to not move from the same place for 24 hours");
+        
+    //test/testtest
+}
+}
